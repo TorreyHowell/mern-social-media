6d73bbb4c234f3a923a0c3d70d02dc291cdb138f
Torrey Howell <torreythowell@gmail.com>
2022-07-15 16:28:21 -0400

    initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+
+const app: Application = express()
+
+const add = (a: number, b: number): number => a + b
+
+app.get('/', (req: Request, res: Response) => {
+  console.log(add(1, 2))
+
+  res.send('Hello')
+})
+
+app.listen(5000, () => console.log('Server running'))
